dd0ccdacba70638832643704a7ae968034b862a0
aguusoleti <agustin_soleti@hotmail.com>
2023-05-01 21:40:14 -0300

    Modificacion del filter

--- BackEnd/src/api/utils/productUtils.js
+++ BackEnd/src/api/utils/productUtils.js
@@ -1,5 +1,11 @@
 import Product from "../models/products.js"
 
 export const findElement = async (element) => {
-  return Product.find(element);
+  console.log(element)
+  const filter ={...element}
+  if("name" in element ) filter.name = {$regex:element.name ,$options: 'i'}
+  if("description" in element ) filter.description = {$regex:element.description ,$options: 'i'}
+
+
+  return Product.find(filter);
 };
